9850ca9ef3f05c6fda2d9be44e7ca3082883aaa0
Mahier Sydow <mahier.sydow8@gmail.com>
2021-10-18 18:29:10 +0900

    first commit

---
+++ variables/variables.js
@@ -0,0 +1,32 @@
+var test = "hello";
+var nb = 8;
+
+// alert(typeof test);
+
+// string concatenation
+console.log("result of nb " + nb);
+
+// can also use , as well
+console.log("result of nb " , nb);
+
+// can use for all calculations
+var result = 1 + 2;
+
+var result = 1 * 2;
+
+var result = 1 - 2;
+
+// a modular, which is used often, basically what is the remainder
+var result = 1%2;
+
+// this will erase the previous result in memory
+result = result + 2
+result +=2; // This is a shortcut for the above
+result -=2;
+result *=2;
+result %=2;
+result /=2;
+
+console.log(result);
+
+
